cc9eabf35bfda76d66beaabf61432664fb603800
anbarasupr <anbarasu.2013@gmail.com>
2020-01-16 00:17:10 +0530

    update

---
+++ 12.spring-rest-crud/3.spring-data-rest-crud/src/main/java/com/spring/dao/EmployeeRepository.java
@@ -0,0 +1,24 @@
+package com.spring.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// that's it ... no need to write any code LOL!
+
+}
+
+
+
+
+
+
+
+
+
+
+
