34ba8895041430eb78f476f41190e9c2c80d695d
anbarasupr <anbarasu.2013@gmail.com>
2020-01-15 23:26:49 +0530

    exec

--- 7.spring-security/6.spring-security-jdbc-schema-modified/src/main/java/hello/Application.java
+++ 7.spring-security/6.spring-security-jdbc-schema-modified/src/main/java/hello/Application.java
@@ -2,11 +2,21 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class Application {
+//@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Throwable {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 }
--- 7.spring-security/6.spring-security-jdbc-schema-modified/src/main/java/hello/WebSecurityConfig.java
+++ 7.spring-security/6.spring-security-jdbc-schema-modified/src/main/java/hello/WebSecurityConfig.java
@@ -3,11 +3,15 @@ package hello;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebMvcSecurity
@@ -23,7 +27,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.usersByUsernameQuery(
 			"select username,password, enabled from users where username=?")
 		.authoritiesByUsernameQuery(
-			"select username, role from user_roles where username=?");
+			"select username, role from user_roles where username=?")
+		.passwordEncoder(passwordEncoder());
 	}	
 	
 	@Override
@@ -42,5 +47,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.and()
 		  .csrf();
 	}
+	
+	@Bean
+	public PasswordEncoder passwordBcryptEncoder() {
+	     return new BCryptPasswordEncoder();
+	}
+	
+	 @Bean
+	 public NoOpPasswordEncoder passwordEncoder() {
+	  return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+	 }
 	
 }
